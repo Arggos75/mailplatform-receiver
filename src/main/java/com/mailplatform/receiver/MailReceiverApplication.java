caa049703598a18176f6acd10f85abd3e7c4830f
orel <a.marchand75@gmail.com>
2026-03-10 09:33:23 +0100

    1

--- src/main/java/com/mailplatform/receiver/MailReceiverApplication.java
+++ src/main/java/com/mailplatform/receiver/MailReceiverApplication.java
@@ -2,8 +2,9 @@ package com.mailplatform.receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MailReceiverApplication {
 
     public static void main(String[] args) {
